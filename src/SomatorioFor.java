b51d92e6726b9b48c1de804178e19fa8670c5013
Jean <jeanmatheus0612@gmail.com>
2024-12-10 21:21:39 -0300

    Arrumando um e começando o primeiro exercicio

---
+++ src/SomatorioFor.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class SomatorioFor {
+    public static void main(String[] args) {
+        int a, b, somatorio, j, i;
+
+        Scanner valor_digitado = new Scanner (System.in);
+
+        System.out.println("Digite um numero: ");
+        a = valor_digitado.nextInt();
+
+        System.out.println("Digite outro numero: ");
+        b = valor_digitado.nextInt();
+
+        somatorio = 0;
+        
+    }
+
+}
