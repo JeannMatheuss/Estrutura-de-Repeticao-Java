3631a1bdd0f6286300f86649842208c0541202af
Jean <jeanmatheus0612@gmail.com>
2024-12-10 21:48:05 -0300

    Finalizando o primeiro exercicio de somatorio usando o for

--- src/SomatorioFor.java
+++ src/SomatorioFor.java
@@ -1,18 +1,27 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class SomatorioFor {
     public static void main(String[] args) {
-        int a, b, somatorio, j, i;
+        int a, b, somatorio;
 
-        Scanner valor_digitado = new Scanner (System.in);
+        Scanner valor_digitado = new Scanner(System.in);
 
-        System.out.println("Digite um numero: ");
+        System.out.println("Digite um número (a): ");
         a = valor_digitado.nextInt();
 
-        System.out.println("Digite outro numero: ");
+        System.out.println("Digite outro número (b): ");
         b = valor_digitado.nextInt();
 
         somatorio = 0;
-        
-    }
 
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= b; j++) {
+                somatorio += (a * b) + Math.pow((j + i), 2);
+            }
+        }
+
+        System.out.println("O resultado do somatório é: " + somatorio);
+
+        valor_digitado.close();
+    }
 }
