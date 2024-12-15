6ac02f4294fe9a9feb53b22219780b3d2459eb9a
Jean <jeanmatheus0612@gmail.com>
2024-12-15 20:46:27 -0300

    Criando as primeiras variaveis e o inicio do loop

--- src/FilaSoldadoFor.java
+++ src/FilaSoldadoFor.java
@@ -1,5 +1,16 @@
+import java.util.*;
 public class FilaSoldadoFor {
     public static void main(String[] args) {
-        
+
+        int quantidade_soldados, i;
+        Scanner dado_digitado = new Scanner (System.in);
+
+        System.out.println("Digite a quantidade de soldados: ");
+        quantidade_soldados = dado_digitado.nextInt();
+
+        for (i = 1; i <= quantidade_soldados; i++) {
+
+        }
     }
+    
 }
