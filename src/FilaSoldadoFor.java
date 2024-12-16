8fbfe4cb80834d92739f5f4f91287545def49f67
Jean <jeanmatheus0612@gmail.com>
2024-12-15 22:07:21 -0300

    Aplicando a logica no codigo

--- src/FilaSoldadoFor.java
+++ src/FilaSoldadoFor.java
@@ -2,14 +2,17 @@ import java.util.*;
 public class FilaSoldadoFor {
     public static void main(String[] args) {
 
-        int quantidade_soldados, i;
+        int quantidade_soldados, i, j;
         Scanner dado_digitado = new Scanner (System.in);
 
         System.out.println("Digite a quantidade de soldados: ");
         quantidade_soldados = dado_digitado.nextInt();
 
         for (i = 1; i <= quantidade_soldados; i++) {
-
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
         }
     }
     
